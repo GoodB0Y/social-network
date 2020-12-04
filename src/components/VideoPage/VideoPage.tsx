ae02cce930145778f6affae79b9081864f727837
iklusovich <ilya.klusovich.89@mail.ru>
2020-12-04 13:12:35 +0300

    test-commit

---
+++ src/components/VideoPage/VideoPage.tsx
@@ -0,0 +1,376 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { uniqueId } from 'lodash';
+import {VideoItem, OpenedVideo} from './index';
+import Slider from '../../common/slider';
+import StyledButton from '../../common/button/Button';
+import arrowNotFilled from '../../common/img/icons/arr_left.svg';
+import PageWrapper from '../../common/pageWrapper';
+import SectionHeader from '../../common/sectionHeader';
+import {
+  addAlbumAction,
+  addVideoAction,
+  AllAlbumAction,
+  allVideosAction,
+} from '../../redux-toolkit/videos/allVideosSlice';
+import 'swiper/swiper.scss';
+import {Controller,  useForm } from 'react-hook-form';
+import { AlbumItem } from './VideoItem';
+import Alert from 'antd/lib/alert';
+
+
+const ComponentWrapper = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap');
+  background: #ffffff;
+  font-family: 'Montserrat', sans-serif;
+  border-radius: 15px;
+  padding: 114px 114px 114px 91px;
+  margin: 275px auto 0 auto;
+  position: relative;
+`;
+
+const PageMarker = styled.h2`
+  margin: 0;
+  left: 90px;
+  top: -91px;
+  padding: 58px 77px;
+  position: absolute;
+  border-radius: 15px;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 39px;
+  background: #ffb11b;
+  color: black;
+`;
+
+const MyVideos = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 63px;
+`;
+
+const Divider =   styled.div`
+  
+  border: 1px solid #000000
+`
+
+const PopularVideos = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const PopularVideosItemWrapper = styled.div`
+  flex-shrink: 0;
+  margin-bottom: 48px;
+  flex-basis: 48%;
+`;
+
+
+
+type OtherDataWrapperType = {
+  show: boolean
+}
+
+const PopularVideoList =  styled.div<OtherDataWrapperType>`
+  flex-wrap: wrap;
+  max-width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  overflow: hidden;
+  transition: all 0.5s;
+  max-height: ${({ show  }) =>
+    (show ? 'auto' : '412px')};
+`;
+
+const ShowHideButton = styled.button<OtherDataWrapperType>`
+  transition: all 0.2s;
+  position: absolute;
+  right: -40px;
+  content: '';
+  border: none;
+  height: 25px;
+  width: 15px;
+  transform: ${({ show }) => 
+   (show ? 'rotate(90deg)' : 'rotate(-90deg)')
+};
+  background: #515151;
+  mask-image: url(${arrowNotFilled});
+  mask-position: center;
+  mask-repeat: no-repeat;
+  cursor: pointer;
+`
+const VideoModalContainer = styled.div`
+  width: 1700px;
+  height: 3000px;
+  z-index: 100;
+  background-color: rgba(0,0,0,0.5);
+  position: absolute;
+  
+  display: block;
+  content:'';
+ h2 {
+    margin-top: 20px;
+    font-weight: bolder
+  }
+  
+`
+
+const  Form = styled.form`
+  width: 800px;
+  border: 3px solid orange;  
+  background-color: white;
+  display: block;
+  position: fixed;
+  top:150px;
+  border-radius: 5px;
+  left: 300px;
+  z-index: 999;  
+  opacity: 1;  
+  box-shadow: 5px 3px 5px #FF6A00;
+  text-align: center;
+ 
+  &  p {
+ color: red;
+ margin: -10px 0 0 0 }
+`;
+
+const Input = styled.input`
+width: 600px;
+display:inline-block;
+border: 1px solid black;
+ border-radius: 5px;
+ cursor: text;
+ margin: 20px;
+ padding: 5px 10px;
+ 
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  
+`
+const Button = styled.button`
+  background-color: #FFB11B;
+  color: white;
+  width: 200px;
+  border-radius: 5px;
+  border: none;
+  height: 40px;
+  margin: 0 20px 20px 0;
+   transition: .5s;
+  &:hover {
+  background-color:#FF6A00;
+  transition: .5s;
+  }
+`
+
+ const VideoPage: React.FC  = () => {
+  // @ts-ignore
+  const {allVideos, allAlbums, error} = useSelector(state => state.videos);
+   const { handleSubmit, errors, control } = useForm({});
+  const [showPopular, setShowPopular] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showModalAlbum, setShowModalAlbum] = useState(false);
+  const [videoToShow, setVideoToShow] = useState({
+    showVideo: false,
+    videoId: '',
+  });
+  const dispatch = useDispatch()
+  useEffect( ()=>{
+    dispatch(allVideosAction(3))
+    dispatch(AllAlbumAction())
+  },[dispatch])
+
+  const showVideo = (id:string) => {
+    setVideoToShow({
+      showVideo: true,
+      videoId: id,
+    });
+  };
+
+  const hideVideo = () => {
+    setVideoToShow({
+      showVideo: false,
+      videoId: '',
+    });
+  };
+// убирает полосу прокрутки при активном видео или при форме добавления
+  useEffect(() => {
+    if (videoToShow.showVideo || showModal) {
+      document.body.style.overflow = 'hidden';
+      return;
+    }
+    document.body.style.overflow = 'unset';
+  }, [videoToShow, showModal]);
+   const  addVideo = async (data:any) =>
+   {
+     await dispatch(addVideoAction(data))
+     setShowModal(false)
+   }
+   const  addAlbum = async (data:any) =>
+   {
+     await dispatch(addAlbumAction(data))
+     setShowModalAlbum(false)
+   }
+  const createModal = (data : any)=>
+    <VideoModalContainer onClick = {()=>{
+      setShowModal(false)
+      setShowModalAlbum(()=> false)
+    }}>
+    <Form method = 'POST' onClick = {(e)=>e.stopPropagation()}>
+      <h2>Добавление нового {showModalAlbum ? 'альбома' : 'видео' } </h2>
+      {!showModalAlbum ?
+        <h3>Для добавления видео необходимо вставить в поля icon и url код, для примера https://www.youtube.com/watch?v=<b>fnzO0U1mSb8</b></h3>
+      : null }
+        {data.map((elem:any)  =>
+        <Controller
+          name={elem.name}
+          control={control}
+          type = {elem.type}
+          key = {elem.name}
+          defaultValue=''
+          rules={{
+            required: elem.rules,
+          }}
+          as={<Input name="name" type={elem.type} placeholder={elem.placeholder}    />}
+
+        />
+      )}
+
+      <ButtonGroup>
+        <Button type="button"  onClick= { handleSubmit( showModalAlbum ? addAlbum : addVideo)} >Ok</Button>
+        <Button type="button" onClick={()=> {
+          setShowModal(()=> false)
+          setShowModalAlbum(()=> false)
+        }
+        }>Cancel</Button>
+      </ButtonGroup>
+    </Form>
+  </VideoModalContainer>
+   return (
+    <>
+
+      {   showModal ? createModal([
+        {
+          name: 'name',
+          placeholder: 'Название видео',
+          type: 'text',
+          rules: 'Введите название видео'
+        },
+        {
+          name: 'author',
+          placeholder: 'Автор видео',
+          type: 'text',
+          rules: 'Введите автора видео'
+        },
+        {
+          name: 'url',
+          placeholder: 'Ссылка на видео',
+          type: 'url',
+          rules: 'Введите ссылку на видео'
+        },
+        {
+        name: 'icon',
+        placeholder: 'Превью для видео',
+        type: 'url',
+        rules: 'Введите ссылку на превью'
+      },
+
+      ]) : null}
+      {   showModalAlbum ? createModal([
+        {
+          name: 'name',
+          placeholder: 'Название альбома',
+          type: 'text',
+          rules: 'Введите название альбома'
+        },
+        {
+        name: 'icon',
+        placeholder: 'Превью для альбома',
+        type: 'url',
+        rules: 'Введите ссылку на превью'
+      },
+
+      ]) : null}
+      {' '}
+      {videoToShow.showVideo ? <OpenedVideo id={videoToShow.videoId} action={hideVideo} /> : null}
+      <PageWrapper>
+        <ComponentWrapper>
+          {' '}
+          <PageMarker>Видеозаписи</PageMarker>
+          <MyVideos>
+            <SectionHeader headline="Мои видео">
+              <StyledButton onClick = {()=>setShowModal((prev)=>!prev)}>Добавить</StyledButton>
+            </SectionHeader>
+            <Slider   slidesToShow={2}  slidesToScroll = {2} loop>
+              {[...allVideos].map((obj) =>
+                (
+                  <VideoItem
+                    key={uniqueId()}
+                    id={obj.id}
+                    url = {obj.url}
+                    name={obj.name}
+                    author={obj.author}
+                    action={() => showVideo(obj.url)}
+                    isPopular={false}
+                  />
+                ))}
+            </Slider>
+          </MyVideos>
+          <Divider />
+          <MyVideos>
+            <SectionHeader headline="Мои альбомы">
+
+              <StyledButton onClick = {()=>setShowModalAlbum((prev)=>!prev)}>Создать</StyledButton>
+            </SectionHeader>
+            {error ? <Alert type = 'error' closable message = {error}/> : null}
+            <Slider   slidesToShow={2}  slidesToScroll = {2} loop>
+              {[...allAlbums].map((obj) =>
+                (
+                  <AlbumItem
+                    key={uniqueId()}
+                    icon = {obj.icon}
+                    name={obj.name}
+                  />
+                ))}
+            </Slider>
+          </MyVideos>
+          <Divider />
+          <PopularVideos>
+            <SectionHeader headline="Популярные" />
+            <PopularVideoList  show={showPopular}>
+              <ShowHideButton
+                show={showPopular}
+                onClick={() =>
+                  setShowPopular(!showPopular)}
+              />
+              {[...allVideos].map((obj) =>
+                (
+                  <PopularVideosItemWrapper key={uniqueId()}>
+                    <VideoItem
+                      key={uniqueId()}
+                      id={obj.id}
+                      url = {obj.url}
+                      author={obj.author}
+                      name={obj.name}
+                      action={() => showVideo(obj.url)}
+                      isPopular={false}
+                    />
+                  </PopularVideosItemWrapper>
+                ))}
+            </PopularVideoList>
+          </PopularVideos>
+        </ComponentWrapper>
+      </PageWrapper>
+   </>
+  );
+};
+
+export default VideoPage
---
+++ src/services/videos-controller/video-controller.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
+import { baseUrlv2 } from '../config';
+
+const baseUrl = `${baseUrlv2}video`
+
+export const fetchVideos = async (page = 2, total = 200): Promise<AxiosResponse> =>
+  axios.get(`${baseUrl}?currentPage=${page}&itemsOnPage=${total}`);
+
+export const fetchAllVideosFromAlbum = async (albumId = 10 ): Promise<AxiosResponse> =>
+  axios.get(`${baseUrl}/album/${albumId}/video`);
+
+export const fetchAddVideoInAlbum = async (albumId = 10, videoId = 13): Promise<AxiosResponse> =>
+  axios.put(`${baseUrl}/album/video?albumId=${albumId}&videoId=${videoId}`);
+
+export const fetchVideoFromName = async (videoName= "Test video 3"): Promise<AxiosResponse> =>
+  axios.get(`${baseUrl}/name?name=${videoName}`);
+
+export const fetchAllAlbum = async (userId = 60): Promise<AxiosResponse> =>
+  axios.get(`${baseUrl}/user/${userId}/album`);
+
+export const fetchAddAlbum = async (userId = 60, data = {}): Promise<AxiosResponse> =>
+  axios.post(`${baseUrl}/user/${userId}/album`, data);
+
+export const fetchGetSomeVideoFromCollection = async (page = 0, count = 15): Promise<AxiosResponse> =>
+  axios.get(`${baseUrl}/user/60/video?currentPage=${page}&itemsOnPage=${count}`);
+
+export const fetchAddVideo = async (userId = 60, data = {}): Promise<AxiosResponse> =>
+  axios.post(`${baseUrl}/user/${userId}/video`, data);
+
