ba28ef1806b98b3779cc11bfc7bff477de4a9c89
PavelBalyakin <pavel.balyakin@gmail.com>
2021-01-11 13:35:15 +0700

    Пофиксит предупреждения ESLint
    Не относится к текущей решаемой задаче,
    просто маленькие улучшения проекта

--- src/stories/photo.stories.tsx
+++ src/stories/photo.stories.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable import/prefer-default-export */
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import PhotoPage from '../components/Photo/Photo';
 
 export default { title: 'Photo Page' };
 
-export const Photo = () => (
+export const Photo = (): JSX.Element => (
   <BrowserRouter>
     <Switch>
       <Route path="*" component={PhotoPage} exact />
