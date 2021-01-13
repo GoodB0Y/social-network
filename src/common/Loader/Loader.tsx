1792051d2d566a7857f74a023efd6a4c67b23f07
PavelBalyakin <pavel.balyakin@gmail.com>
2021-01-13 16:08:34 +0700

    Рефакторинг компонента

--- src/common/Loader/Loader.tsx
+++ src/common/Loader/Loader.tsx
@@ -1,10 +1,27 @@
-import * as React from 'react';
-import StyledLoading from './styles';
+import React from 'react';
+import styled from 'styled-components';
+import loader from './loader.svg';
 
-interface ILoadingBlock {
-  size?: number;
-}
+export type LoaderProps = { size?: number };
 
-const Loader: React.FC<ILoadingBlock> = ({ size = 150 }) => <StyledLoading $size={size} />;
+const Container = styled.div<{ $size: number }>`
+  margin: 0 auto;
+  width: ${({ $size }) => $size}px;
+  height: ${({ $size }) => $size}px;
+  background-image: url(${loader});
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const Loader = ({ size = 150 }: LoaderProps): JSX.Element => <Container $size={size} />;
+
+// delete after add
+// "react/require-default-props": ["error", { "ignoreFunctionalComponents": true }],
+// in .eslintrc.json
+// https://github.com/okjel/social-network/pull/14#discussion_r556243552
+Loader.defaultProps = {
+  size: 150,
+};
 
 export default Loader;
