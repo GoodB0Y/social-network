a9c1738f4588e02b3ff7270dbaf24af8f789ae15
Artyom <kiel.artyom@gmail.com>
2021-01-14 03:31:18 +0800

    refactor(AudioPage): вынесены стили в отдельный файл

---
+++ src/components/Audio/AudioPage/AudioPage.styles.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  //width: 1300px;
+  //height: 1000vh;
+`;
+
+export const SliderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  z-index: 2;
+`;
+
+export const TitleWrapper = styled.div`
+  margin: 0 60px;
+`;
+
+export const LeftSide = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  div {
+    margin-right: 27px;
+  }
+`;
+
+export const RightSide = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
