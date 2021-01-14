7769794b47dbc7dcad53a1f13e4af98d601e5442
Artyom <kiel.artyom@gmail.com>
2021-01-14 14:02:12 +0800

    feat(Audio): добавлен стори для комопонента FilterTabs

---
+++ src/components/Audio/FilterTabs/FilterTabs.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import FilterTabs, { FilterTabsProps, Tabs } from './FilterTabs';
+
+export default {
+  title: 'FilterTabs',
+  component: FilterTabs,
+} as Meta;
+
+const Template: Story<FilterTabsProps> = (args) => <FilterTabs {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  activeTab: Tabs.All,
+  openTab: (tab) => tab,
+};
