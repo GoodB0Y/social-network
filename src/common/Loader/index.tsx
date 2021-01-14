93880e58ff41a24d3bd631c0059eec88fe0f4870
PavelBalyakin <pavel.balyakin@gmail.com>
2021-01-14 11:47:59 +0700

    Вынесет стили в отдельный файл

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-import loader from './assets/loader.svg';
+import Container from './Loader.styles';
 
 export type LoaderProps = { size?: number };
-
-const Container = styled.div<LoaderProps>`
-  margin: 0 auto;
-  width: ${({ size }) => size}px;
-  height: ${({ size }) => size}px;
-  background-image: url(${loader});
-  background-size: contain;
-  background-position: center;
-  background-repeat: no-repeat;
-`;
 
 const Loader = ({ size = 150 }: LoaderProps): JSX.Element => <Container size={size} />;
 
