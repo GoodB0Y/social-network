eba39aef0ef50902e7acb47a8b3fdcb91d14c85f
Egor1408 <egor1408@yandex.ru>
2021-01-17 01:57:51 +0300

    рефактор Bokkmarks

--- src/components/Bookmarks/index.tsx
+++ src/components/Bookmarks/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import Page from '../../common/Page';
-import BookMarksPage from './components';
+import { PageMarker } from '../Friends/Friends';
+import BlockNotes from '../Main/Articles/blockNotes/BlockNotes';
 
-function Bookmarks() {
-  return (
-    <Page>
-      <BookMarksPage />
-    </Page>
-  );
-}
+const BookmarksWrapper = styled.div`
+  background: #fff;
+  font-family: 'Montserrat', sans-serif;
+  border-radius: 15px;
+  padding: 114px;
+  margin-top: 275px;
+  position: relative;
+  min-height: 1200px;
+`;
+
+const Bookmarks: React.FC = () => (
+  <Page>
+    <BookmarksWrapper>
+      <PageMarker>Закладки</PageMarker>
+      <BlockNotes />
+    </BookmarksWrapper>
+  </Page>
+);
 
 export default Bookmarks;
