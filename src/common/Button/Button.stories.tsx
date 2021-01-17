845c7667efa27e48ded63278ded7e13f6e9ce774
PavelBalyakin <pavel.balyakin@gmail.com>
2021-01-17 21:44:56 +0700

    Добавит сторис

--- src/common/Button/Button.stories.tsx
+++ src/common/Button/Button.stories.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import Button from './index';
+import { Meta, Story } from '@storybook/react';
+import Button, { ButtonProps } from './index';
+import { ButtonProps as StyledButtonProps } from './Button.styles';
 
-export default { title: 'Button' };
+export default {
+  title: 'Common/Button',
+  component: Button,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  },
+} as Meta;
 
-export const ButtonSmall = (): JSX.Element => (
-  <Button type="button" small>
-    ButtonLabel
-  </Button>
+const Template: Story<ButtonProps & StyledButtonProps> = (args): JSX.Element => (
+  <Button {...args} />
 );
 
-export const ButtonMedium = (): JSX.Element => <Button type="button">ButtonLabel</Button>;
+export const Small = Template.bind({});
+Small.args = {
+  label: 'Вступить в группу',
+  size: 'small',
+};
 
-export const ButtonWide = (): JSX.Element => (
-  <Button type="button" wide>
-    ButtonLabel
-  </Button>
-);
+export const Medium = Template.bind({});
+Medium.args = {
+  label: 'Зарегистрироваться',
+  size: 'medium',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  label: 'Добавить',
+  size: 'large',
+};
