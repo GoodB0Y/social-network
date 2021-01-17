4183cc9dfa9c8dc2bb75328d5109f05ff42a8f06
PavelBalyakin <pavel.balyakin@gmail.com>
2021-01-17 21:38:08 +0700

    Рефакторинг стилей

--- src/common/Button/Button.styles.ts
+++ src/common/Button/Button.styles.ts
@@ -1,28 +1,36 @@
 import styled from 'styled-components';
+import { ButtonStyles, defaultSize } from './consts';
 
-const Button = styled.button<{ small?: boolean; wide?: boolean }>`
+export type ButtonProps = {
+  size?: 'small' | 'medium' | 'large';
+};
+
+const Button = styled.button<ButtonProps>`
   display: block;
-  background: #ffb11b;
+  padding: ${({ size = defaultSize }) => ButtonStyles.padding[size]};
   border: none;
-  font-family: 'Montserrat', sans-serif;
-  font-style: normal;
-  letter-spacing: 0.065em;
-  font-weight: ${(props) => (props.small ? '400' : '500')};
-  font-size: ${(props) => (props.small ? '16px' : '24px')};
-  border-radius: ${(props) => (props.small ? '5px' : '15px')};
-  width: ${(props) => (props.wide ? '100%' : null)};
-  line-height: ${(props) => (props.small ? '20px' : '29px')};
-  padding: ${(props) => (props.small ? '15px 40px' : '19px 66px')};
+  border-radius: ${({ size = defaultSize }) => ButtonStyles.borderRadius[size]};
+  font-family: inherit;
+  font-size: ${({ size = defaultSize }) => ButtonStyles.fontSize[size]};
+  font-weight: ${({ size = defaultSize }) => ButtonStyles.fontWeight[size]};
+  line-height: ${({ size = defaultSize }) => ButtonStyles.lineHeight[size]};
+  letter-spacing: ${({ size = defaultSize }) => ButtonStyles.letterSpacing[size]};
+  text-transform: ${({ size = defaultSize }) => ButtonStyles.textTransform[size]};
+  background-color: #ffb11b;
+  color: #000000;
   transition: all 0.2s ease-out;
+
   &:hover {
     cursor: pointer;
     box-shadow: 0 0 0 3px #ffffff, 0 0 0 5px #ffb11b;
   }
+
   &:active {
     outline: none;
     box-shadow: 0 0 0 3px #ffffff, 0 0 0 5px #000000;
     transition: none;
   }
+
   &:focus {
     outline: none;
   }
