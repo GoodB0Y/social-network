847ad3924d0c429d92a9be9fa7a027ffedfda4a9
tronintimur <tronintl@yandex.ru>
2021-02-04 23:08:49 +0400

    Добавит горячую загрузку для redux devTools

--- src/redux-toolkit/store.ts
+++ src/redux-toolkit/store.ts
@@ -5,6 +5,12 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./rootReducer', () => {
+    store.replaceReducer(rootReducer);
+  });
+}
+
 export type TypeDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof rootReducer>;
 
