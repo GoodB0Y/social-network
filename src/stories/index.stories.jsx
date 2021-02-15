61a8b7faf2e3a1c12b7dd3ce6b957b59decddfdb
Anna Kuzn <annbuho@gmail.com>
2021-02-15 14:42:56 +0300

    Удалит неиспользуемые файлы
    Вместо всего содержимого папки Articles теперь используется компонент ArticleMenu

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Entry from '../components/Entry';
-import BlockNotes from '../components/Main/Articles';
 import UserInfoHeader from '../components/Main/UserInfoHeader';
 import Wall from '../components/Main/Wall';
 import WallCreateArticle from '../components/Main/WallCreateArticle';
@@ -16,7 +15,6 @@ import {
 storiesOf('Social network', module)
   .add('Страница авторизации', () => <Entry />)
   .add('Страница сообщений', () => <MessagesPage />)
-  .add('Блок записей', () => <BlockNotes />)
   .add('Аватар', () => <UserInfoHeader />)
   .add('Стена', () => <Wall />)
   .add('Создание записи на стене', () => <WallCreateArticle />)
