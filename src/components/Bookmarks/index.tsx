44e3c34f31e70942556f3d513121ddf0c42f064e
Anna Kuzn <annbuho@gmail.com>
2021-02-15 14:40:10 +0300

    Изменит Bookmarks
    Вместо использования списка статей из компонента главной страницы, будет использоваться компонент ArticlesMenu

--- src/components/Bookmarks/index.tsx
+++ src/components/Bookmarks/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { connect, ConnectedProps } from 'react-redux';
+
+import { RootState } from '../../redux-toolkit/store';
+import { menuItemsNames } from '../../common/ArticlesMenu/menuItemsData';
+
+import ArticlesMenu from '../../common/ArticlesMenu/ArticlesMenu';
 import Page from '../../common/Page';
 import Chip from '../../common/Chip';
-import BlockNotes from '../Main/Articles';
-import { BookmarksWrapper, PageMarker } from './Bookmarks.styles';
 
-const Bookmarks: React.FC = () => (
-  <Page>
-    <BookmarksWrapper>
-      <Chip>Закладки</Chip>
-      <BlockNotes />
-    </BookmarksWrapper>
-  </Page>
-);
+const BookmarksWrapper = styled.div`
+  background: #fff;
+  font-family: 'Montserrat', sans-serif;
+  border-radius: 15px;
+  padding: 114px;
+  margin-top: 275px;
+  position: relative;
+  min-height: 1200px;
+`;
 
-export default Bookmarks;
+const mapStateToProps = (state: RootState) => ({
+  user: state.user,
+});
+
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const Bookmarks = ({ user }: PropsFromRedux): JSX.Element => {
+  const { all, myNotes, recommend } = menuItemsNames;
+
+  return (
+    <Page>
+      <BookmarksWrapper>
+        <Chip>Закладки</Chip>
+        <ArticlesMenu itemsNames={[all, myNotes, recommend]} userId={user?.data?.userId} />
+      </BookmarksWrapper>
+    </Page>
+  );
+};
+
+export default connector(Bookmarks);
