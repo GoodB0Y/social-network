a294911e4388d2d2f7b21b77f1e5006892cd2084
Anna Kuzn <annbuho@gmail.com>
2021-02-18 15:55:39 +0300

    Исправит отображение заглушки аватарки
    Добавит обработку загрузки изображения. В случае  ошибки загрузки будет установлена заглушка.

--- src/common/Avatar/Avatar.styles.ts
+++ src/common/Avatar/Avatar.styles.ts
@@ -8,7 +8,6 @@ const Container = styled.img<AvatarProps>`
   height: ${({ size }) => size}px;
   max-width: 100%;
   max-height: 100%;
-  background-image: url(${({ path }) => path});
   border-radius: 50%;
   box-shadow: 9px 9px 25px rgba(0, 0, 0, 0.25);
   background-size: cover;
--- src/common/Avatar/index.tsx
+++ src/common/Avatar/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './Avatar.styles';
 import nophoto from './assets/nophoto.png';
 
-export type AvatarProps = { size?: number; path?: string };
+export type AvatarProps = { size?: number; src?: string; onError?: () => void };
 
-const Avatar = ({ size = 150, path = nophoto }: AvatarProps): JSX.Element => (
-  <Container size={size} path={path || nophoto} />
-);
+const Avatar = ({ size = 150, src = nophoto }: AvatarProps): JSX.Element => {
+  const [path, setPath] = useState(src);
+
+  return <Container size={size} src={path} onError={() => setPath(nophoto)} />;
+};
 
 export default Avatar;
