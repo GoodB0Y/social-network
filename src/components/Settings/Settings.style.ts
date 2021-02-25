b9dfb15b429ec38335d72053cee112d5d4d47a7a
Igor Kovtun <kovtun.ig100@gmail.com>
2021-02-26 00:03:06 +0300

    Добавляет обертку и заголовок для стр. Settings

--- src/components/Settings/Settings.style.ts
+++ src/components/Settings/Settings.style.ts
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+const SettingsWrapper = styled.div`
+  background: #fff;
+  font-family: 'Montserrat', sans-serif;
+  border-radius: 15px;
+  padding: 114px;
+  margin-top: 275px;
+  position: relative;
+  min-height: 1200px;
+`;
+
+export default SettingsWrapper;
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
 import Page from '../../common/Page';
+import Chip from '../../common/Chip';
+import SettingsWrapper from './Settings.style';
 
 const Settings: React.FC = () => {
   const { register, handleSubmit, errors, setError } = useForm();
 
   return (
     <Page>
-      <div>Hello</div>
+      <SettingsWrapper>
+        <Chip>Настройки</Chip>
+      </SettingsWrapper>
     </Page>
   );
 };
