5e0cb7259c3c9ba82ac52d83f477e73444316e27
Igor Kovtun <kovtun.ig100@gmail.com>
2021-02-26 22:40:46 +0300

    Добавит форму в Settings

--- src/components/Settings/Settings.style.ts
+++ src/components/Settings/Settings.style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const SettingsWrapper = styled.div`
+export const SettingsWrapper = styled.div`
   background: #fff;
   font-family: 'Montserrat', sans-serif;
   border-radius: 15px;
@@ -10,4 +10,74 @@ const SettingsWrapper = styled.div`
   min-height: 1200px;
 `;
 
-export default SettingsWrapper;
+export const ContainerLabel = styled.div`
+  font-family: Montserrat, serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 22px;
+  color: #000;
+  padding: 0 0 40px;
+`;
+
+export const Container = styled.div`
+  min-height: 66px;
+  padding-bottom: 40px;
+`;
+
+export const Label = styled.label`
+  font-family: Montserrat, serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  color: #000;
+  padding: 0 20px 0 0;
+  min-width: 11em;
+  display: inline-block;
+`;
+
+export const UserInput = styled.input`
+  min-width: 37em;
+  border: none;
+  border-bottom: 2px solid #000;
+  color: #000;
+  &:hover,
+  &:focus,
+  &:active {
+    outline: 0;
+    outline-offset: 0;
+    border-bottom: 2px solid #ffb11b;
+  }
+`;
+
+export const Select = styled.select`
+  min-width: 7em;
+  border: none;
+  border-bottom: 2px solid #000;
+  color: #000;
+  font-family: Montserrat, serif;
+  font-style: normal;
+  font-weight: 500;
+  &:hover,
+  &:focus,
+  &:active {
+    outline: 0;
+    outline-offset: 0;
+    border-bottom: 2px solid #ffb11b;
+  }
+`;
+
+export const Birthday = styled.input`
+  min-width: 3em;
+  border: none;
+  border-bottom: 2px solid #000;
+  color: #000;
+  &:hover,
+  &:focus,
+  &:active {
+    outline: 0;
+    outline-offset: 0;
+    border-bottom: 2px solid #ffb11b;
+  }
+`;
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import Page from '../../common/Page';
 import Chip from '../../common/Chip';
-import SettingsWrapper from './Settings.style';
+import {
+  SettingsWrapper,
+  UserInput,
+  Label,
+  Container,
+  Select,
+  Birthday,
+  ContainerLabel,
+} from './Settings.style';
 
 const Settings: React.FC = () => {
   const { register, handleSubmit, errors, setError } = useForm();
@@ -11,6 +19,85 @@ const Settings: React.FC = () => {
     <Page>
       <SettingsWrapper>
         <Chip>Настройки</Chip>
+        <form>
+          {/* Основные */}
+          <ContainerLabel>Основные</ContainerLabel>
+          <Container>
+            <Label htmlFor="name">Имя</Label>
+            <UserInput id="name" type="text" placeholder="Имя" />
+          </Container>
+          <Container>
+            <Label htmlFor="surname">Фамилия</Label>
+            <UserInput id="surname" type="text" placeholder="Фамилия" />
+          </Container>
+          <Container>
+            <Label htmlFor="sex">Пол</Label>
+            <Select name="sex">
+              <option disabled>Выберите пол</option>
+              <option value="Мужской">Мужской</option>
+              <option value="Женский">Женский</option>
+            </Select>
+          </Container>
+          <Container>
+            <Label htmlFor="birthday">День рождения</Label>
+            <Birthday id="day" type="text" placeholder="День" />
+            <Birthday id="mounth" type="text" placeholder="Месяц" />
+            <Birthday id="year" type="text" placeholder="Год" />
+          </Container>
+          <Container>
+            <Label htmlFor="showBirthday"> </Label>
+            <Select name="showBirthday">
+              <option disabled>Показывать дату рождения</option>
+              <option value="Да">Показывать дату рождения</option>
+              <option value="Частично">Показывать только дату и месяц</option>
+              <option value="Нет">Не показывать дату рождения</option>
+            </Select>
+          </Container>
+
+          {/* Авторизация */}
+          <ContainerLabel>Авторизация</ContainerLabel>
+          <Container>
+            <Label htmlFor="email">Email</Label>
+            <UserInput id="email" type="email" placeholder="E-mail" />
+          </Container>
+          <Container>
+            <Label htmlFor="password">Новый пароль</Label>
+            <UserInput id="password" type="password" placeholder="Новый пароль" />
+          </Container>
+          <Container>
+            <Label htmlFor="newpassword">Повторите пароль</Label>
+            <UserInput id="newpassword" type="password" placeholder="Повторите новый пароль" />
+          </Container>
+
+          {/* Контакты */}
+          <ContainerLabel>Контакты</ContainerLabel>
+          <Container>
+            <Label htmlFor="country">Страна</Label>
+            <UserInput id="country" type="text" placeholder="Страна" />
+          </Container>
+          <Container>
+            <Label htmlFor="city">Город</Label>
+            <UserInput id="city" type="text" placeholder="Город" />
+          </Container>
+          <Container>
+            <Label htmlFor="phone">Моб. телефон</Label>
+            <UserInput id="phone" type="tel" name="phone" placeholder="Моб. телефон" />
+          </Container>
+
+          {/* О себе */}
+          <ContainerLabel>О себе</ContainerLabel>
+          <Container>
+            <Label htmlFor="specialization">Специализация</Label>
+            <Select id="specialization" name="specialization">
+              <option disabled>Я в JM</option>
+              <option value="Frontand">Frontand-разработчик</option>
+              <option value="Backend">Backend-разработчик</option>
+              <option value="Mentor">Ментор</option>
+            </Select>
+            <Label htmlFor="specialization">Комментарий</Label>
+            <textarea> </textarea>
+          </Container>
+        </form>
       </SettingsWrapper>
     </Page>
   );
