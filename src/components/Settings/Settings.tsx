d87ba4a9b268a9f14f4244e916b1751d83c479ab
Igor Kovtun <kovtun.ig100@gmail.com>
2021-03-06 13:19:19 +0300

    Добавит блок Authorization в форму

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,13 +3,13 @@ import { useForm } from 'react-hook-form';
 import Page from '../../common/Page';
 import Chip from '../../common/Chip';
 import UserBlock from './form/userBlock';
+import AuthBlock from './form/authBlock';
 import {
   SettingsWrapper,
   UserInput,
   Label,
   Container,
   Select,
-  Birthday,
   ContainerLabel,
   TextArea,
 } from './Settings.style';
@@ -26,19 +26,7 @@ const Settings: React.FC = () => {
           <UserBlock />
 
           {/* Авторизация */}
-          <ContainerLabel>Авторизация</ContainerLabel>
-          <Container>
-            <Label htmlFor="email">Email</Label>
-            <UserInput id="email" type="email" placeholder="E-mail" />
-          </Container>
-          <Container>
-            <Label htmlFor="password">Новый пароль</Label>
-            <UserInput id="password" type="password" placeholder="Новый пароль" />
-          </Container>
-          <Container>
-            <Label htmlFor="newpassword">Повторите пароль</Label>
-            <UserInput id="newpassword" type="password" placeholder="Повторите новый пароль" />
-          </Container>
+          <AuthBlock />
 
           {/* Контакты */}
           <ContainerLabel>Контакты</ContainerLabel>
---
+++ src/components/Settings/form/authBlock/AuthBlock.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { UserInput, Label, Container, ContainerLabel } from '../../Settings.style';
+
+const AuthBlock: React.FC = () => (
+  <>
+    <ContainerLabel>Авторизация</ContainerLabel>
+    <Container>
+      <Label htmlFor="email">Email</Label>
+      <UserInput id="email" type="email" placeholder="E-mail" />
+    </Container>
+    <Container>
+      <Label htmlFor="password">Новый пароль</Label>
+      <UserInput id="password" type="password" placeholder="Новый пароль" />
+    </Container>
+    <Container>
+      <Label htmlFor="newpassword">Повторите пароль</Label>
+      <UserInput id="newpassword" type="password" placeholder="Повторите новый пароль" />
+    </Container>
+  </>
+);
+
+export default AuthBlock;
